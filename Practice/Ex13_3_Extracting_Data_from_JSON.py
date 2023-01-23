05ef0d976ab7a860d6c2cf5b1a501a60c7cf0edc
Sandipgorasiya99 <111065759+Sandipgorasiya99@users.noreply.github.com>
2023-01-23 10:51:16 +0100

    Update and rename Ex13_3.py to Ex13_3_Extracting_Data_from_JSON.py

--- Practice/Ex13_3_Extracting_Data_from_JSON.py
+++ Practice/Ex13_3_Extracting_Data_from_JSON.py
@@ -7,7 +7,7 @@ ctx = ssl.create_default_context()
 ctx.check_hostname= False
 ctx.verify_mode= ssl.CERT_NONE
 
-url = input ('Enter URL: ')
+url = 'http://py4e-data.dr-chuck.net/comments_1609183.json'
 print('Retriving', url)
 uh = urllib.request.urlopen(url,context=ctx)
 
